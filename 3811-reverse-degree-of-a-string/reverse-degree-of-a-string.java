c8402ab51ebc1b68e59aee3d603d01c216531152
varunan k.m <varunksm878@gmail.com>
2026-03-24 15:57:12 +0530

    Time: 1 ms (100.00%) | Memory: 44 MB (75.55%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
@@ -1,16 +1,11 @@
 class Solution {
     public int reverseDegree(String s) {
-
-        int sum = 0;
-
+      int sum = 0;
         for(int i = 0; i < s.length(); i++){
             char ch = s.charAt(i);
-
-            int reversePos = 'z' - ch + 1;
-
-            sum += reversePos * (i + 1);
+             int reversePos = 'z' - ch + 1;
+             sum += reversePos * (i + 1);
         }
-
         return sum;
     }
 }
