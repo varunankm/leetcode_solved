3e16d4b87750cee46c1cc891accaded40087f32f
varunan k.m <varunksm878@gmail.com>
2026-04-14 08:14:13 +0530

    Time: 1 ms (100.00%) | Memory: 46.2 MB (77.54%) - LeetSync

---
+++ 2844-sum-of-squares-of-special-elements/sum-of-squares-of-special-elements.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int sumOfSquares(int[] nums) {
+     int n=nums.length;
+     int ans=0;
+   
+     for(int i=0;i<nums.length;i++){
+        if(n%(i+1)==0){
+           ans+=nums[i]*nums[i];
+        }
+     }
+   
+     return ans;   
+    }
+}
