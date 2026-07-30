f1102c96400ca36a2db100ac0b95df9c26ed77e5
varunan k.m <varunksm878@gmail.com>
2026-07-30 10:25:39 +0530

    Time: 51 ms (19.04%) | Memory: 47.1 MB (98.30%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.java
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.java
@@ -1,16 +1,30 @@
 class Solution {
     public int minSteps(String s, String t) {
-       int ch[]=new int[26];
-       for(int i=0;i<s.length();i++){
-        ch[s.charAt(i)-'a']++;
-        ch[t.charAt(i)-'a']--;
-       }
-       int count=0;
-       for(int n:ch){
-        if(n>0){
-            count+=n;
+    //    int ch[]=new int[26];
+    //    for(int i=0;i<s.length();i++){
+    //     ch[s.charAt(i)-'a']++;
+    //     ch[t.charAt(i)-'a']--;
+    //    }
+    //    int count=0;
+    //    for(int n:ch){
+    //     if(n>0){
+    //         count+=n;
+    //     }
+    //    }
+    //   return count;
+    int count=0;
+    HashMap<Character,Integer> map=new HashMap<>();
+    for(char c:s.toCharArray()){
+        map.put(c,map.getOrDefault(c,0)+1);
+    }
+      for(char c:t.toCharArray()){
+        map.put(c,map.getOrDefault(c,0)-1);
+    }
+    for(char c:map.keySet()){
+        if(map.get(c)>0){
+            count+=map.get(c);
         }
-       }
-      return count;
+    }
+    return count;
     }
 }
